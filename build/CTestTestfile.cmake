907df8d28696a7b668939ffc83a4977f82904c1c
andystevenson910 <andystevenson910@gmail.com>
2024-10-14 10:49:26 -0600

    fixed the compiler

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(UnitTest "UnitTestRunner")
-set_tests_properties(UnitTest PROPERTIES  _BACKTRACE_TRIPLES "/home/andy/Desktop/array-performance/CMakeLists.txt;22;add_test;/home/andy/Desktop/array-performance/CMakeLists.txt;0;")
+set_tests_properties(UnitTest PROPERTIES  _BACKTRACE_TRIPLES "/home/andy/Desktop/array-performance/CMakeLists.txt;26;add_test;/home/andy/Desktop/array-performance/CMakeLists.txt;0;")
